d421e70df12c95bf57dbb45000666dd91dd1b431
Matthew Yoon <83928330+matthewsyoon@users.noreply.github.com>
2023-03-28 01:01:21 -0400

    Add files via upload

---
+++ scripts/load.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function () {
+  $("#navbar-placeholder").load("html/navbar.html");
+  $("#hero-placeholder").load("html/hero.html");
+  $("#about-placeholder").load("html/about.html");
+});
